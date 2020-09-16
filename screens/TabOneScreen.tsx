2889a8e26a069761435f0cad7affe59f02e06b8a
yy zhang <mycodegit@gmail.com>
2020-09-16 10:35:26 +0800

    -4 添加了ramda库, 完成配置式的改造

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,43 +1,27 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import * as d3 from 'd3';
 import { arc } from 'd3-shape';
 import { Circle, Svg, Rect, Path } from 'react-native-svg';
 import styled from 'styled-components';
+import * as R from 'ramda';
+
+
+
+const colorArr: string[] = ['#da2923', '#f8c300', '#65baa3', '#07f919', '#fbfbcc']
+const arcParams: setArcType[] = [{ innerD: 0.65, outerD: 0.70, sA: -Math.PI / 4, eA: 1.6 * Math.PI, cR: 1 }, { innerD: 0.702, outerD: 0.75, sA: -Math.PI / 4, eA: 1.7 * Math.PI, cR: 1 }, { innerD: 0.752, outerD: 0.802, sA: -Math.PI / 4, eA: 1.7 * Math.PI, cR: 1 }, { innerD: 0.804, outerD: 0.852, sA: -Math.PI / 4, eA: 1.7 * Math.PI, cR: 1 }, { innerD: 0.854, outerD: 0.902, sA: -Math.PI / 4, eA: 1.7 * Math.PI, cR: 1 }]
+
 export default function TabOneScreen() {
-  const [path1, setPath1] = React.useState<string | undefined>(undefined)
-  const [path2, setPath2] = React.useState<string | undefined>(undefined)
-  const [path3, setPath3] = React.useState<string | undefined>(undefined)
-  const [path4, setPath4] = React.useState<string | undefined>(undefined)
-  const path = false;
-
-  const colorArr: string[] = ['#da2923', '#f8c300', '#65baa3', '#07f919', '#fbfbcc']
-
+  const [PathArr, setPathArr] = React.useState<string[]>([])
   React.useEffect(() => {
-
-    // const backgroundArc = d3.arc()
-    //   .innerRadius(0.65)
-    //   .outerRadius(0.70)
-    //   .startAngle(-Math.PI / 4)
-    //   .endAngle(1.6 * Math.PI)
-    //   .cornerRadius(1)()
-
-    const backgroundArc1 = getArc({ innerD: 0.65, outerD: 0.70, sA: -Math.PI / 4, eA: 1.6 * Math.PI, cR: 1 });
-    const backgroundArc2 = getArc({ innerD: 0.702, outerD: 0.75, sA: -Math.PI / 4, eA: 1.7 * Math.PI, cR: 1 });
-    const backgroundArc3 = getArc({ innerD: 0.752, outerD: 0.802, sA: -Math.PI / 4, eA: 1.7 * Math.PI, cR: 1 });
-    const backgroundArc4 = getArc({ innerD: 0.804, outerD: 0.852, sA: -Math.PI / 4, eA: 1.7 * Math.PI, cR: 1 });
-
-    setPath1(backgroundArc1);
-    setPath2(backgroundArc2);
-    setPath3(backgroundArc3);
-    setPath4(backgroundArc4);
+    const res: string[] = []
+    arcParams.map((arc, index) => {
+      let r = getArc(arc)()
+      res.push(r);
+      setPathArr(res);
+    })
   }, []);
-
-  console.log(path1);
-
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
@@ -49,36 +33,19 @@ export default function TabOneScreen() {
       >
         <Svg height="50%" width="50%" viewBox={[-1, -1, 2, 2].join(" ")}>
 
-          {path ? (<Text>Loading...</Text>) : (<>
-            <Path d={path1} fill={colorArr[0]}></Path>
-            <Path d={path2} fill={colorArr[1]}></Path>
-            <Path d={path3} fill={colorArr[2]}></Path>
-            <Path d={path4} fill={colorArr[4]}></Path>
-          </>)
+          {
+            PathArr.map((path: string, index: number) => {
+              return (
+                <Path d={path} fill={colorArr[index]} key={index}></Path>
+              )
+            })
           }
-
         </Svg>
+
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
 
 type circleType = {
   cx: string;
@@ -121,6 +88,25 @@ const getArc = ({ innerD = 0.65, outerD = 0.70, sA = -Math.PI / 4, eA = 1.6 * Ma
     .cornerRadius(cR)
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
 
 
 
